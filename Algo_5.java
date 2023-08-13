c8df2c1825a1c885b88656748e8af09c5841538f
Sanduni Haththotuwa <sandunihaththotuwa@gmail.com>
2023-08-13 11:10:23 +0530

    Complete Algo-5

--- Algo_5.java
+++ Algo_5.java
@@ -5,13 +5,11 @@ public class Algo_5 {
 
     private static final Scanner scanner =new Scanner(System.in);
     public static void main(String[] args) {
+        String input="";
 do{
         System.out.print("Enter text here: ");
-
-
-        String input = scanner.nextLine().strip();
-        if(input=="")System.out.println("Invalid input");
-        else{
+        input = scanner.nextLine().strip();
+} while(input.isBlank());          
         char[] chars = input.toCharArray();
         int i=0;
         char temp ;
@@ -23,9 +21,9 @@ do{
 
         }
          System.out.println(Arrays.toString(chars));
-     }
      
-     } while(true);   
+
+   
 
     }
     
