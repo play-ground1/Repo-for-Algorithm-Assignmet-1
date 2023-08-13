b1903405a8a28ae4de118f70fb7f2a209584780e
Sanduni Haththotuwa <sandunihaththotuwa@gmail.com>
2023-08-13 10:22:46 +0530

    Complete Algo-5

---
+++ Algo_5.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Algo_5 {
+
+    private static final Scanner scanner =new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.print("Enter text here: ");
+
+        String input = scanner.nextLine().strip();
+        
+        char[] chars = input.toCharArray();
+        int i=0;
+        char temp ;
+        while(i<chars.length/2){
+            temp = chars[i];
+            chars[i]=chars[chars.length-1-i];
+            chars[chars.length-1-i]= temp;
+            i++;
+
+        }
+            System.out.println(Arrays.toString(chars));
+    }
+    
+}
